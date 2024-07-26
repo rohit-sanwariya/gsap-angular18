8da8939e436025995c26ad2667af40aa3c95445a
rohit-sanwariya <rohitsanwariya1995@gmail.com>
2024-07-26 15:55:59 +0530

    ease back working.

--- src/app/Pages/gsap-to/gsap-to.component.ts
+++ src/app/Pages/gsap-to/gsap-to.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, signal } from '@angular/core';
 import { GoBackButtonComponent } from "../../Components/go-back-button/go-back-button.component";
-
+import {gsap} from 'gsap';
 @Component({
   selector: 'app-gsap-to',
   standalone: true,
@@ -8,6 +8,40 @@ import { GoBackButtonComponent } from "../../Components/go-back-button/go-back-b
   templateUrl: './gsap-to.component.html',
   styleUrl: './gsap-to.component.scss'
 })
-export class GsapToComponent {
+export class GsapToComponent implements AfterViewInit {
+  gsapToList = signal<Array<{ease:gsap.EaseString,id:'blue-box'| 'blue-box-ease-back-out' | 'blue-box-ease-back-in'|'blue-box-ease-back-in-out'}>>([
+    
+    {
+      ease:'back.in',id:'blue-box-ease-back-in'
+    },
+    {
+      ease:'back.inOut',id:'blue-box-ease-back-in-out',
+    },
+    {
+      ease:'back.out',id:'blue-box-ease-back-out'
+    },
+    {
+      ease:'none',id:'blue-box'
+    }
+  ])
+  ngAfterViewInit(): void {
+    this.gsapToList().forEach((value)=>{
+      const box = document.getElementById(value.id);
+      gsap.to(box,{
+        ...structuredClone(DEFAULT_CONFIG),ease:value.ease
+         });  
+    })
+  
+  }
+   
 
+
+}
+
+const DEFAULT_CONFIG : gsap.TweenVars = {
+  x:500,
+  repeat:-1,
+  duration:4,
+  yoyo:true,
+  delay:4,
 }
